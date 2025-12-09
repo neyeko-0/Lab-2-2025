816d6db9f7e4aa8e9117cf03ecfd646646190713
neyeko-0 <second.gakmail@gmail.com>
2025-12-09 17:30:41 +0400

    Lab2 Кораблев Данила Сергеевич 6203-010302D
    Lab2 Кораблев Данила Сергеевич 6203-010302D

---
+++ Main.java
@@ -0,0 +1,48 @@
+import functions.FunctionPoint;
+import functions.TabulatedFunction;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        double [] data = {5, 2, 8, 2, 1};
+        FunctionPoint Point = new FunctionPoint(0.1, 5);
+        TabulatedFunction Function = new TabulatedFunction(0.0,1.0, data);
+        System.out.println("Input data:");
+        for (int i = 0; i < Function.getPointsCount(); i++){
+            System.out.println("X["+ i + "] = " + Function.getPointX(i) + " and Y["+ i + "] = " + Function.getPointY(i));
+        }
+        Function.deletePoint(4);
+        System.out.println("\n");
+        System.out.println("As a result of deleting:");
+        for (int i = 0; i < Function.getPointsCount(); i++){
+            System.out.println("X["+ i + "] = " + Function.getPointX(i) + " and Y["+ i + "] = " + Function.getPointY(i));
+        }
+        Function.addPoint(Point);
+        System.out.println("\n");
+        System.out.println("As a result of adding:");
+        for (int i = 0; i < Function.getPointsCount(); i++){
+            System.out.println("X["+ i + "] = " + Function.getPointX(i) + " and Y["+ i + "] = " + Function.getPointY(i));
+        }
+        System.out.println("\n");
+        System.out.println("Function value in the given X:0.3  Y = " + Function.getFunctionValue(0.3));
+       
+        System.out.println("\n");
+        System.out.println("As a result of Setting and getting:");
+        for(int i=0; i<Function.getPointsCount();i++)
+        {
+            Function.setPointX(i,i);
+            Function.setPointY(i,i);
+        }
+        
+        for(int i=0; i<Function.getPointsCount();i++)
+        {
+            System.out.println("X = " + Function.getPointX(i) + " and Y = " + Function.getPointY(i));
+
+        }
+        
+        
+
+    }
+
+}
---
+++ functions/TabulatedFunction.java
@@ -0,0 +1,140 @@
+package functions;
+public class TabulatedFunction {
+    
+    private FunctionPoint points[];
+    private int pointsCount;
+
+    public TabulatedFunction(double leftX, double rightX, int pointsCount) {
+        points = new FunctionPoint[pointsCount];
+        double step = (rightX - leftX) / (pointsCount - 1);
+        for (int i = 0; i < pointsCount; i++) {
+            points[i] = new FunctionPoint(leftX + i * step, 0);
+        }
+
+    }
+
+    public TabulatedFunction(double leftX, double rightX, double[] values) {
+        pointsCount = values.length;
+        points = new FunctionPoint[pointsCount];
+        double step = (rightX - leftX) / (pointsCount - 1);
+        for (int i = 0; i < pointsCount; i++) {
+            points[i] = new FunctionPoint(leftX + i * step, values[i]);
+        }
+    }
+
+    public double getLeftDomainBorder() {
+        return points[0].getX();
+    }
+
+    public double getRightDomainBorder() {
+        return points[pointsCount-1].getX();
+    }
+
+    public double getFunctionValue(double x)
+     {
+        double epcilon=1e-9;
+        double leftX=points[0].getX();
+        double rightX=points[pointsCount-1].getX();
+        if (x < leftX|| x > rightX)
+            return Double.NaN;
+        if(Math.abs(x-leftX)<epcilon)
+        {
+            return points[0].getY();
+        }
+        if(Math.abs(x-rightX)<epcilon)
+        {
+            return points[pointsCount-1].getY();
+        }
+        int i = 0;
+        double value=0;
+        for (i=0;x > points[i].getX();++i)
+        {
+        value = points[i].getY() + (points[i + 1].getY() - points[i].getY()) * (x - points[i].getX()) / (points[i + 1].getX() - points[i].getX());
+        }
+        return value;
+
+    }
+
+    public int getPointsCount() {
+        return pointsCount;
+    }
+
+    public FunctionPoint getPoint(int index) {
+        if(index<0||index>=pointsCount)
+        {
+            return null;
+        }
+        return points[index];
+    }
+
+    public void setPoint(int index, FunctionPoint point) {
+        if (index < 0 || index >= pointsCount)
+            return;
+        points[index] = new FunctionPoint(point);
+    }
+
+    public double getPointX(int index) {
+
+        if (index < 0 || index >= pointsCount)
+            return Double.NaN;
+        return points[index].getX();
+    }
+
+
+    public void setPointX(int index, double x) {
+        if (index < 0 || index > pointsCount)
+            return;
+        points[index].setX(x);
+        
+    }
+
+    public double getPointY(int index) {
+        if (index < 0 || index > pointsCount)
+            return Double.NaN;
+        return points[index].getY();
+    }
+
+    public void setPointY(int index, double y) {
+        points[index].setY(y);
+    }
+
+  public void deletePoint(int index){
+    if (index < 0 || index >= pointsCount){
+            return;
+        } 
+        
+        if(index == pointsCount - 1){
+            pointsCount--;
+            points[pointsCount] = null;
+        }
+        else{ 
+            System.arraycopy(points, index + 1, points, index, pointsCount - 1 - index);
+            pointsCount--;
+            points[pointsCount] = null;
+        }
+        
+    
+        }
+
+    public void addPoint(FunctionPoint point) {
+        int i = 0;
+        double epcilon=1e-9;
+
+    while (i < pointsCount && Math.abs(point.getX()-points[i].getX())<epcilon) {
+        i++;
+    }
+
+   
+    if (i < pointsCount && Math.abs(point.getX()-points[i].getX())<epcilon) {
+        setPointY(i, point.getY());
+        return;
+    }
+
+    
+    System.arraycopy(points, i, points, i + 1, pointsCount - i);
+    points[i] = new FunctionPoint(point);
+    pointsCount++;
+}
+
+
+}
